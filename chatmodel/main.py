5395712bd6197a22d7e955fa0a66b42b695c573d
Poras Nehra <porasnehra43@gmail.com>
2026-03-08 23:48:41 +0530

    Add files via upload

---
+++ chatmodel/main.py
@@ -0,0 +1,23 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+api_key = os.getenv('GOOGLE_API_KEY')
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash', api_key=api_key)
+
+chat_history = [
+    SystemMessage(content='You are a helpful AI assistant')
+]
+
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+
+print(chat_history)
